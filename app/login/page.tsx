0dee8bbc4ac6b0edcd1866d14e1c080322a6b75b
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-09-11 04:45:23 -0300

    update url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login({
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: "https://personal-portfolio-mgc.vercel.app/auth/callback",
       },
     });
 
