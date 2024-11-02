2a4c7faff11bc9263e025f91aa94156acf309708
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-01 23:54:24 -0300

    some fixies

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,9 @@
-export const Loader = () => {
+export const Loader = ({ className }: { className: string }) => {
   return (
     <div className="flex w-[100%] h-[100%] p-[1.5px]">
-      <span className="loader border-t-neutral-400/50 border-l-neutral-400/50 border-r-neutral-400/50 border-b-violet-400/60 border-[6px]"></span>
+      <span
+        className={`loader ${className} border-t-neutral-400/50 border-l-neutral-400/50 border-r-neutral-400/50 border-b-violet-400/60 border-[6px]`}
+      ></span>
     </div>
   );
 };
