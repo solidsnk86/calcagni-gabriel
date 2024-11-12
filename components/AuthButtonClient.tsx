d4989c4399572acbbe043d441b2ee6e0af519978
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-11 23:21:28 -0300

    update loaders in buttons

--- components/AuthButtonClient.tsx
+++ components/AuthButtonClient.tsx
@@ -4,12 +4,42 @@ import { LogOut } from "lucide-react";
 import React from "react";
 import { AuthButtonProps } from "@/app/types/definitions";
 import useMatchMedia from "@/app/hooks/useMatchMedia";
+import { useFormStatus } from "react-dom";
+import { Loader } from "./Loader";
+
+// Componente separado para el botón de submit
+const LogoutButton = ({ mobile }: { mobile: boolean }) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="py-2 px-4 flex items-center rounded-md no-underline bg-btn-background hover:bg-btn-background-hover border border-foreground/5"
+      title="Cerrar Sesión"
+      type="submit"
+    >
+      {mobile ? (
+        ""
+      ) : pending ? (
+        <>
+          <span className="mr-1">Cerrando..</span>
+          <Loader width="20" height="20" />
+        </>
+      ) : (
+        "Cerrar Sesión"
+      )}
+      {pending ? null : (
+        <LogOut className={`${mobile ? "ml-0" : "ml-2"} w-5 h-5 inline`} />
+      )}
+    </button>
+  );
+};
 
 export const AuthButtonClient: React.FC<AuthButtonProps> = ({
   signOut,
   user,
 }) => {
   const mobile = useMatchMedia("(max-width: 700px)", false);
+
   return (
     <div className="flex items-center gap-4">
       <div className="flex items-center gap-2">
@@ -22,18 +52,8 @@ export const AuthButtonClient: React.FC<AuthButtonProps> = ({
         />
         <span className="hidden md:block">{user.user_metadata.user_name}!</span>
       </div>
-      <form>
-        <button
-          className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover border border-foreground/5"
-          title="Cerrar Sesión"
-          onClick={async (e) => {
-            e.preventDefault();
-            await signOut();
-          }}
-        >
-          {mobile ? "" : "Cerrar Sesión"}
-          <LogOut className={`${mobile ? "ml-0" : "ml-2"} w-5 h-5 inline`} />
-        </button>
+      <form action={signOut}>
+        <LogoutButton mobile={mobile} />
       </form>
     </div>
   );
