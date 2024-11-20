34b28fb3e9f292ab2104eaadc9121ec966604d1a
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-19 22:08:09 -0300

    handle amount of pictures in swiper

--- components/main-section/Section-4.tsx
+++ components/main-section/Section-4.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { ArrowLeft, ArrowRight, CameraIcon } from "lucide-react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Setcion_4Props } from "@/app/types/definitions";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, EffectCards, Keyboard } from "swiper/modules";
@@ -8,6 +10,12 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 export const Section_4: React.FC<Setcion_4Props> = ({ className }) => {
+  const [value, setValue] = useState<number>(0);
+
+  const handleSlideChange = (swiper: any) => {
+    setValue(swiper.activeIndex);
+  };
+
   return (
     <section
       className={`border border-foreground/5 bg-zinc-900/50 rounded-xl overflow-hidden ${className}`}
@@ -26,6 +34,9 @@ export const Section_4: React.FC<Setcion_4Props> = ({ className }) => {
         >
           <ArrowLeft />
         </button>
+        <small className="-translate-y-2">
+          {value + 1} de {photosImg.length}
+        </small>
         <button
           className="swiper-button-next mx-4 -translate-y-3 p-[2px] bg-zinc-800 border-zinc-800/50 rounded-full"
           title="Siguiente"
@@ -44,15 +55,14 @@ export const Section_4: React.FC<Setcion_4Props> = ({ className }) => {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
           }}
+          onSlideChange={handleSlideChange}
           className="mySwiper"
         >
-          {photosImg.map((photo, i) => {
-            return (
-              <SwiperSlide key={i + 1}>
-                <img src={photo.link} />
-              </SwiperSlide>
-            );
-          })}
+          {photosImg.map((photo, i) => (
+            <SwiperSlide key={i + 1}>
+              <img src={photo.link} alt={`Foto ${i + 1}`} />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </aside>
     </section>
