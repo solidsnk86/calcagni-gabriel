5eaa752bfa6288322466f8ca3956a95160d939f2
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-26 16:38:01 -0300

    updated headers for the api

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/non-followers",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
