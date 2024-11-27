40acc27ea9cbb38e2085b79ef1e5331df21beaaf
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-27 20:36:24 -0300

    Link

--- components/header-components/Section-4.tsx
+++ components/header-components/Section-4.tsx
@@ -98,9 +98,9 @@ export const Section_4 = ({ className }: { className?: string }) => {
           >
             <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
           </svg>
-          <a href="mailto:calcagni.gabriel86@gmail.com" target="_blank">
+          <Link href="mailto:calcagni.gabriel86@gmail.com" target="_blank">
             Email
-          </a>
+          </Link>
         </FancyButton>
         <FancyButton
           className={`py-3 px-3 inline-flex items-center justify-center w-[100%] text-center ${
