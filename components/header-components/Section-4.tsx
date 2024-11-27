143f6a5a3a0f8c677ef6ff3ade9580023d6086d1
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-27 06:10:03 -0300

    changed photo profile

--- components/header-components/Section-4.tsx
+++ components/header-components/Section-4.tsx
@@ -16,7 +16,7 @@ export const Section_4 = ({ className }: { className?: string }) => {
     >
       <header className="flex gap-4">
         <Image
-          src="/avatar-mgc.webp"
+          src="/avatar-mgc.png"
           width={100}
           height={100}
           alt="Avatar Gabriel 🎃"
