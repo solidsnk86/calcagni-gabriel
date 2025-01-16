5d8dde3f91bee75e7480a3387ba91482d3e94512
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-16 04:46:44 -0300

    updated haversine location api

---
+++ app/api/haversine-geolocation/route.ts
@@ -0,0 +1,65 @@
+import { haversine } from '@/utils/haversine-formula';
+import { NextRequest } from 'next/server';
+
+const getAllAirports = async () => {
+  const response = await fetch(
+    'https://cdn.jsdelivr.net/gh/liquidsnk86/cdn-js@main/world-airports.json'
+  );
+  const airports = await response.json();
+  const filteredData = Object.keys(airports)
+    .map((key) => {
+      const { iata, name, city, country, lat, lon } = airports[key];
+      return iata ? { iata, name, city, country, lat, lon } : null;
+    })
+    .filter(Boolean);
+  return filteredData;
+};
+
+export async function GET(req: NextRequest) {
+  const lat = parseFloat(req.nextUrl.searchParams.get('lat') || '0');
+  const lon = parseFloat(req.nextUrl.searchParams.get('lon') || '0');
+
+  if (!lat || !lon) {
+    return Response.json(
+      { message: 'You must to provide latitude and longitude params' },
+      { status: 400 }
+    );
+  }
+  const coords = { lat, lon };
+  try {
+    const airports = await getAllAirports();
+
+    const getAirport = (coordinates: any, airports: any) => {
+      let closestAirport = null;
+      let minMeters = Infinity;
+
+      for (const airport of airports) {
+        const meters = haversine(coordinates, airport);
+
+        if (meters < minMeters) {
+          minMeters = meters;
+          closestAirport = airport;
+        }
+      }
+
+      return { closestAirport, minMeters };
+    };
+    const { closestAirport, minMeters } = getAirport(coords, airports);
+    return Response.json(
+      {
+        message: 'success',
+        city: closestAirport.city,
+        country: closestAirport.country,
+        airport: closestAirport.name,
+        airport_coords: {
+          longitude: closestAirport.lon,
+          latitude: closestAirport.lat,
+        },
+        airport_distance: `${minMeters.toFixed(2)}km`,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    return Response.json({ message: 'Server Error', error_message: err });
+  }
+}
---
+++ utils/haversine-formula.ts
@@ -0,0 +1,24 @@
+const EARTH_RAIDUS = 6378137;
+const square = (num: number) => num * num;
+const degreesToRadians = (degrees: number) => (degrees * Math.PI) / 180.0;
+
+type Coords = {
+  lat: number;
+  lon: number;
+};
+
+export const haversine = (locationA: Coords, locationB: Coords) => {
+  const latitudeA = degreesToRadians(locationA.lat);
+  const latitudeB = degreesToRadians(locationB.lat);
+  const longitudeA = degreesToRadians(locationA.lon);
+  const longitudeB = degreesToRadians(locationB.lon);
+  // Fórmula de Haversine
+  const formula =
+    square(Math.sin((latitudeB - latitudeA) / 2)) +
+    Math.cos(latitudeA) *
+      Math.cos(latitudeB) *
+      square((longitudeB - longitudeA) / 2);
+  // Calcular distancia usando la fórmula de Haversine
+  const distance = 2 * EARTH_RAIDUS * Math.asin(Math.sqrt(formula));
+  return distance / 1000;
+};
