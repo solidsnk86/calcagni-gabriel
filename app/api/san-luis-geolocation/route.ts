be9e8c191933a77e2a2693a200f2a552335015e4
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-20 05:11:47 -0300

    updated query params

--- app/api/san-luis-geolocation/route.ts
+++ app/api/san-luis-geolocation/route.ts
@@ -49,16 +49,7 @@ const getAllAntennas = async () => {
 export async function GET(req: NextRequest) {
   const lat = parseFloat(req.nextUrl.searchParams.get('lat') || '0');
   const lon = parseFloat(req.nextUrl.searchParams.get('lon') || '0');
-  const query = req.nextUrl.searchParams.get('query' || 'WiFi3.0-CO-28');
-  const response = NextResponse.next();
-
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Allow-Methods', 'GET');
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization'
-  );
-  response.headers.set('Access-Control-Max-Age', '86400');
+  const query = req.nextUrl.searchParams.get('query');
 
   if (!lat || !lon) {
     return Response.json(
@@ -86,20 +77,22 @@ export async function GET(req: NextRequest) {
     };
 
     const searchAntenna = (coordinates: any, allData: any, query: any) => {
-      let target = null;
       let targetDistance = Infinity;
       let searchedTarget = null;
+      let coords = { latitude: 0, longitude: 0 };
 
       for (const data of allData) {
         const distance = haversine(coordinates, data);
 
         if (query === data.name) {
           searchedTarget = data.name;
-          targetDistance = Number(distance.toFixed(4));
+          targetDistance = Number(distance.toFixed(3));
+          coords.latitude = data.lat;
+          coords.longitude = data.lon;
         }
       }
 
-      return { target, targetDistance, searchedTarget };
+      return { targetDistance, searchedTarget, coordinates };
     };
 
     const [cities, antennas] = await Promise.all([
@@ -111,16 +104,16 @@ export async function GET(req: NextRequest) {
     const { closestTarget, minDistance } = getClosest(coords, antennas);
 
     if (query) {
-      const { target, targetDistance, searchedTarget } = searchAntenna(
+      const { targetDistance, searchedTarget, coordinates } = searchAntenna(
         coords,
         antennas,
         query
       );
       return Response.json({
         searched: {
-          antenna: target,
-          distance: `${targetDistance}mts`,
-          searched: searchedTarget,
+          antenna: searchedTarget,
+          distance: `${targetDistance}km`,
+          coords: coordinates,
         },
       });
     }
