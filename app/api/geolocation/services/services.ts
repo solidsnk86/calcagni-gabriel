5ce776a29758fb05ae34fb86f18ab69beb47a63f
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-01-21 15:19:30 -0300

    updated url

--- app/api/geolocation/services/services.ts
+++ app/api/geolocation/services/services.ts
@@ -2,7 +2,7 @@ import { haversine } from '@/utils/haversine-formula';
 
 const getAllCities = async () => {
   const resposne = await fetch(
-    'https://cdn.jsdelivr.net/gh/liquidsnk86/cdn-js@main/geodata-ar.json'
+    'https://cdn.jsdelivr.net/gh/liquidsnk86/cdn-js@main/geodata-arg.json'
   );
   const jsonData = await resposne.json();
   const formatJSON = Object.keys(jsonData).map((key) => {
