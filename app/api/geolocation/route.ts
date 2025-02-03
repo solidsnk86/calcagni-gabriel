2ea3a0647247fead476b9c2f57d80aac0beba436
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-02-03 18:25:36 -0300

    remove json

--- app/api/geolocation/route.ts
+++ app/api/geolocation/route.ts
@@ -55,7 +55,7 @@ export async function GET(req: NextRequest) {
           targetDistance === Infinity ? 'No disponible ' : targetDistance
         }mts`,
         coords: coordinates || 'No disponible',
-        MAC: mac || 'MAC no disponible',
+        MAC: mac || 'MAC 2.4G no disponible',
         MAC5G: mac5 || 'MAC 5G no disponible',
       });
     }
