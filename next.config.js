1c0238cc38d681bc16b7c5a109314559d6df5a57
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-22 23:47:00 -0300

    testing api

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://neo-wifi.vercel.app',
+            value: '*',
           },
         ],
       },
